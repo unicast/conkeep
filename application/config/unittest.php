bfa92dd8da44bb69641c175eac997dca177662ca
Ilya Kozlov <unicast.ru@gmail.com>
2011-05-02 00:07:15 +0400

    cleaning some trash

--- application/config/unittest.php
+++ application/config/unittest.php
@@ -18,7 +18,7 @@ return array(
 
 	// If you don't use a whitelist then only files included during the request will be counted
 	// If you do, then only whitelisted items will be counted
-	'use_whitelist' => FALSE,
+	'use_whitelist' => TRUE,
 
 	// Items to whitelist, only used in cli
 	// Web runner ui allows user to choose which items to whitelist
@@ -31,11 +31,11 @@ return array(
 		// Set to array(TRUE) to include all modules, or use an array of module names
 		// (the keys of the array passed to Kohana::modules() in the bootstrap)
 		// Or set to FALSE to exclude all modules
-		'modules' => array(TRUE),
+		'modules' => array(FALSE),
 
 		// If you don't want the Kohana code coverage reports to pollute your app's,
 		// then set this to FALSE
-		'system' => TRUE,
+		'system' => FALSE,
 	),
 
 	// Does what it says on the tin
